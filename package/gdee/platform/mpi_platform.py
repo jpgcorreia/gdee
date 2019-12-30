2f646f266947ca71924f91d481e997fc61768112
Caio S. Souza <cssouza@itqb.unl.pt>
2019-12-30 19:28:26 +0000

    Added MPI platform
    Simplified MPIRunner requests
    
    Added MPI platform

--- package/gdee/platform/mpi_platform.py
+++ package/gdee/platform/mpi_platform.py
@@ -1,38 +1,97 @@
 """
 """
+from mpi4py import MPI
+from enum import IntEnum, auto
 
 
-class RunnerState:
-    def __init__(self):
-        pass
+class RequestType(IntEnum):
+    get_task = 0
+    save = auto()
+    run_task = auto()
+    terminate = auto()
 
 
 class Message:
-    def __init__(self):
-        pass
+    def __init__(self, request, data=None):
+        self.request = request
+        self.data = data
 
 
 class MPIPlatform:
     def __init__(self, pipeline):
         self.pipeline = pipeline
-        print("MPI platform created")
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.root = 0
+        self.manager = None
+        self.runner = None
 
     def run(self):
-        print("Running on MPI platform")
-        self.pipeline.run_initial()
+        if self.rank == self.root:
+            self.manager = MPIManager(self.comm, self.pipeline)
+            self.manager.run()
 
-        while self.pipeline.available():
-            job_data = self.pipeline.next_job()
-            results = self.pipeline.run_pipeline(job_data)
-            self.pipeline.save_results(results)
+        else:
+            self.runner = MPIRunner(self.comm, self.root, self.pipeline)
+            self.runner.run()
 
 
 class MPIManager:
-    def __init__(self):
-        pass
+    def __init__(self, mpi_comm, pipeline):
+        self.comm = mpi_comm
+        self.rank = mpi_comm.Get_rank()
+        self.status = MPI.Status()
+        self.pipeline = pipeline
+        self.alive = mpi_comm.Get_size() - 1 # Number of runners
+        self.not_saved = 0
+
+    def run(self):
+        # Run non-parallel tasks
+        self.pipeline.run_initial()
+
+        while self.alive or self.not_saved:
+            message = self.comm.recv(source=MPI.ANY_SOURCE, status=self.status)
+
+            if message.request == RequestType.get_task:
+                self.send_task()
+
+            if message.request == RequestType.save:
+                self.pipeline.save_results(message.data)
+                self.not_saved -= 1
+
+    def send_task(self):
+        if self.pipeline.available():
+            task = self.pipeline.next_job()
+            message = Message(RequestType.run_task, task)
+            self.not_saved += 1
+
+        else:
+            message = Message(RequestType.terminate)
+            self.alive -= 1
+
+        runner = self.status.Get_source()
+        self.comm.send(message, dest=runner)
 
 
 class MPIRunner:
-    def __init__(self):
-        pass
+    def __init__(self, mpi_comm, root, pipeline):
+        self.comm = mpi_comm
+        self.rank = mpi_comm.Get_rank()
+        self.root = root
+        self.pipeline = pipeline
 
+    def run(self):
+        message = self.request_task()
+
+        while message.request != RequestType.terminate:
+            results = self.pipeline.run_pipeline(message.data)
+            self.comm.send(Message(RequestType.save, results), dest=self.root)
+
+            message = self.request_task()
+
+    def request_task(self):
+        return self.comm.sendrecv(
+            Message(RequestType.get_task),
+            dest=self.root,
+            source=self.root
+        )
