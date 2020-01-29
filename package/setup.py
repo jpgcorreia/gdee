631ed9874242ae52ef17d74d2b3df7cc5170127b
Caio S. Souza <cssouza@itqb.unl.pt>
2020-01-29 17:30:00 +0000

    Updated requirements

--- package/setup.py
+++ package/setup.py
@@ -35,5 +35,5 @@ setup(
     package_dir = {"gdee": "gdee"},
     ext_package = "gdee",
     # ext_modules = extensions,
-    install_requires = ["numpy>=1.14", "mdanalysis>=0.20", "mpi4py>=3.0", "path>=13.0"],
+    install_requires = ["numpy>=1.14", "mdanalysis>=0.20", "mpi4py>=3.0", "path>=13.0", "biopython>=1.75"],
 )
