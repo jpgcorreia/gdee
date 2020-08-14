2a4c5c09bcb28f6eb4db266b69d05fbc15d92c01
Caio S. Souza <souzacaiobsb@gmail.com>
2020-08-14 18:00:35 +0100

    Bug: wrong PDB format

--- package/gdee/evaluator/pdbqt.py
+++ package/gdee/evaluator/pdbqt.py
@@ -77,7 +77,7 @@ class PDBQT:
                 fd.write("MODEL {:d}\nREMARK ENERGY: {:.2f}\n".format(model_idx, model.energy))
 
                 for atom_idx in range(len(self.atom_data)):
-                    fd.write("{:<29s}{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}   {}\n".format(
+                    fd.write("{:<30s}{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}   {}\n".format(
                         self.atom_data[atom_idx],
                         model.coords[atom_idx, 0],
                         model.coords[atom_idx, 1],
